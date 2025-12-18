3f9859b38786bd90f60964adebf1ce78eff37e0d
Carlos Abea <carlosabea91@gmail.com>
2025-12-17 18:03:36 -0600

    Hola

---
+++ EjerciciosHolaMundo/ejercicio.py
@@ -0,0 +1,18 @@
+#Este es el ejercicio 1
+print("Hola mundo")
+
+#Este es el ejercicio 2
+#El codigo del ejercicio 1 lo que hace es imprimir con la funcion print el hola mundo el cual es una cadena de texto
+
+#Este es el ejercicio 3
+print("Carlos Alfredo Abea Martinez y mi edad es de 18 años")
+
+#Este es el ejercicio 4
+print(type("Esta es una cadena de texto"))
+print(type(1))
+print(type(1.12))
+
+#Este es el ejercicio 5
+"""Los tipos de datos en python son datos ya prederterminados de python, existen varios como el String, el int, el float, el double
+el complex, el bool entre otros
+"""
---
+++ hello.py
@@ -0,0 +1,22 @@
+#Hola mundo, this is my first world
+#Hola mundo
+print("This is my first hello world")
+print("This is my second hello python and im gonna be able to learn how to do ai by myself I swear")
+
+#print(type("este se utiliza para ver que tipo de datos esta dentro de type, en este caso imprimiria str, like string"))
+print(type("Hello world Im learning how to code in Python"))
+#Ejemplos de consultas de datos
+print(type(1)) #class type int
+print(type(1.12))#class type float and so on 
+print(type(False))
+print(type(1 + 2j)) #type compex
+
+
+
+"""
+Este es un comentario 
+en varias lienas
+"""
+"""
+Este es mi otro omenrario de varias lineas 
+"""
