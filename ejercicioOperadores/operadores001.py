702b8fa746ea2d05829d015aa395a4b89cad2894
Carlos Abea <carlosabea91@gmail.com>
2025-12-19 17:48:13 -0600

    m

---
+++ ejercicioOperadores/operadores001.py
@@ -0,0 +1,36 @@
+
+###Operadores Aritmeticos###
+print(3 + 4)
+print(3 - 4)
+print(3 / 4)
+print(3 * 4)
+print(10 % 2)
+print(2**3)
+
+print("Hola " + "Python")
+print("Hola " + str(5))
+
+print("Hola " * 5)
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+###Operadores Comparativos###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print( 4 != 4)
+
+
+print("Hola" > "Python ")
+
+
+###Operadores Logicos###
+
+print(3 > 4 and "Hola" > "Python") 
+print(3 > 4  or "Hola" > "Python") 
+print(not(3 > 4))
+#print(3 > 4 not "Hola" > "Python") 
